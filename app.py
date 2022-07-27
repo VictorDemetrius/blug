ee4370db7e6b6f80ddabf785c11268ae635e8fe2
Victor Demetrius Marqueto <victor.demetrius@gmail.com>
2022-07-27 00:36:53 +0000

    Aula html

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
-
+from datetime import datetime
 app = Flask("Hello")
 
-@app.route("/")
-@app.route("/hello")
-def hello():
-    return "Fala guerreiro"
+posts = [
 
-@app.route("/meucontato")
-def meuContato():
-    return render_template('index.html')
+            {
+                "title": "FIM DOS TEMPOS",
+                "body" : " O mundo vai acabar. E vai ser dia 30/02",
+                "author" : "O Vidente",
+                "created" : datetime (2012,12,25)
+            },
+            {
+                "title": "Era tudo MENTIRA!!!",
+                "body" : " Vidente mentiu era um charlatão, supera",
+                "author" : "O Detetive",
+                "created" : datetime (2012,12,26)
+            }
+
+        ]
+@app.route("/")
+def index():
+    return render_template("index.html", posts = posts)
+
+
