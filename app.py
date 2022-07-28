57aa470e226f86fbc68123376f35f7c19549b3ed
Victor Demetrius Marqueto <victor.demetrius@gmail.com>
2022-07-28 00:35:04 +0000

    Aula de banco de dados

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
-app = Flask("Hello")
+from flask_sqlalchemy import SQLAlchemy
 
-posts = [
+app = Flask("hello")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-            {
-                "title": "FIM DOS TEMPOS",
-                "body" : " O mundo vai acabar. E vai ser dia 30/02",
-                "author" : "O Vidente",
-                "created" : datetime (2012,12,25)
-            },
-            {
-                "title": "Era tudo MENTIRA!!!",
-                "body" : " Vidente mentiu era um charlatão, supera",
-                "author" : "O Detetive",
-                "created" : datetime (2012,12,26)
-            }
+db = SQLAlchemy(app)
 
-        ]
+class Post(db.Model):
+    __tablename__= 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(1000))
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class Useer(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True )
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author')
+
+db.create_all()
+
 @app.route("/")
 def index():
+    posts = Post.query.all()
     return render_template("index.html", posts = posts)
+
+@app.route("/populate")
+def populate():
+    user = User(username ='FantasminhaSapeca' , email='alem@ceu.com', password='Buuu' )
+    post1 = Post(title="PostFantasma", body="Buuuuuuuuuu", author=user)
+    post2 = Post(title="PostResposta", body="Vá de reto satanás", author=user)
+    db.session.add(user)
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
